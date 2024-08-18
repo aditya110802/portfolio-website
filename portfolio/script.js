662c9ee4ef22b45e23e6a2b7694fece45a1c98c2
aditya110802 <143443882+aditya110802@users.noreply.github.com>
2024-08-18 07:50:47 +0530

    Replaced old code with new and made modifications.

--- portfolio/script.js
+++ portfolio/script.js
@@ -3,6 +3,32 @@ let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
+function SendMail(e){
+    e.preventDefault(); // Prevent form submission
+
+    console.log("SendMail function called");
+
+    var params = {
+       from_name: document.getElementById("name").value,
+       email_id: document.getElementById("email").value,
+       phone_no: document.getElementById("phone").value,
+       subject_emailjs: document.getElementById("subject").value,
+       message: document.getElementById("message").value
+    }
+
+    console.log("Params: ", params);
+
+    emailjs.send("service_13z4gxe", "template_7brabsm", params)
+    .then(function(res) {
+       alert("Success! " + res.status);
+    })
+    .catch(function(error) {
+       console.error("Failed to send email: ", error);
+       alert("Failed to send message. Please try again later.");
+    });
+}
+
+
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
@@ -24,5 +50,12 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 };
 
-//       service_508dmuq
-//       template_y7vl94a
+
+document.getElementById('hire-btn').addEventListener('click', function(e) {
+    e.preventDefault(); // Prevent default anchor behavior
+    var copyText = document.getElementById('email-copy');
+    copyText.select();
+    document.execCommand('copy');
+    alert('Email address copied! You can now send me a message. Looking forward to hearing from you!');
+});
+
